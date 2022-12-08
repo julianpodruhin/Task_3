2fcc5ebe83abb53c7497268af82ced18beda47e4
julianpodruhin <yulianpodruhin@mail.ru>
2022-12-08 13:22:23 +0300

    Initial commit

---
+++ Task_3.py
@@ -0,0 +1,15 @@
+
+import math
+import statistics
+import random
+
+numbers = list(range(0,10))
+print(sum(numbers))
+
+print (sum(numbers)/len(numbers))
+print (statistics.mean(numbers))
+print (statistics.median(numbers))
+print(statistics.stdev(numbers))
+print(numbers)
+print(random.randint(0,100))
+
